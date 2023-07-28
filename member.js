3747ca08cb49e7969ece21b9cc138a197b3c1648
Caroline Corrêa <32968047+carolsc@users.noreply.github.com>
2023-07-28 02:12:44 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember()
+{
+    var member = new Member(); // Create a new member object
+    member.skills = ["JavaScript", "Java", "C#", "Python", "PHP", "Ruby", "C++", "C", "Go", "Swift", "Objective-C", "SQL", "Scala", "R", "MATLAB", "Perl", "Visual Basic", "Groovy", "Assembly", "CoffeeScript", "TypeScript", "Lua", "Haskell", "Clojure", "Erlang", "F#", "Rust", "Dart", "Julia", "Elixir", "OCaml", "Scheme", "Smalltalk", "PowerShell", "Bash", "Kotlin", "Delphi", "Awk", "Fortran", "Lisp", "Prolog", "Ada", "COBOL", "Forth", "LabVIEW", "Ladder Logic", "Verilog", "VHDL", "ABAP", "Apex", "AutoLISP", "BASIC", "Caml", "Chapel", "Eiffel", "Fantom", "Forth", "FoxPro", "Icon", "IDL", "J", "JScript", "JScript.NET", "Ladder Logic", "Lasso", "Limbo", "LotusScript", "M", "ML", "Modula-2", "Modula-3", "MUMPS", "NATURAL", "Oberon", "OpenCL", "OpenEdge ABL", "PL/I", "PostScript", "PowerBuilder", "Progress", "REXX", "S", "S-PLUS", "SAS", "Sather", "Simula", "SPARK", "Tcl", "Turing", "VBScript", "X++"];
+    return
+    {
+        member: member
+    };
+}
